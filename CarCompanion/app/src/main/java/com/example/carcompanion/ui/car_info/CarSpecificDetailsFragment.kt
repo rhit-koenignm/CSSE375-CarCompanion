d63f98cdaec6b5b3d632c61c89ccee738a0497c0
ChronicApp <koenignm@rose-hulman.edu>
2023-03-09 14:24:57 -0500

    Commented out code in CarSpecificDetailsFragment to compile project

--- CarCompanion/app/src/main/java/com/example/carcompanion/ui/car_info/CarSpecificDetailsFragment.kt
+++ CarCompanion/app/src/main/java/com/example/carcompanion/ui/car_info/CarSpecificDetailsFragment.kt
@@ -1,4 +1,4 @@
-package edu.rosehulman.kaupaies.carcompanion.ui.car_info
+package com.example.carcompanion.ui.car_info
 
 import android.os.Bundle
 import android.util.Log
@@ -9,32 +9,38 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import edu.rosehulman.kaupaies.carcompanion.Constants
-import edu.rosehulman.kaupaies.carcompanion.R
+import com.example.carcompanion.Constants
+import com.example.carcompanion.databinding.FragmentCarDetailsBinding
+import edu.rosehulman.kaupaies.carcompanion.ui.car_info.CarDetails
 import edu.rosehulman.kaupaies.carcompanion.ui.car_info.diagnosis_section.DiagnosisAdapter
 import edu.rosehulman.kaupaies.carcompanion.ui.troubleshooting.TroubleAdapter
 
 class CarSpecificDetailsFragment (private val car: CarDetails): Fragment() {
 
+    private lateinit var binding: FragmentCarDetailsBinding
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_car_specific_details, container, false)
+        binding = FragmentCarDetailsBinding.inflate(inflater, container, false)
+        //val view = inflater.inflate(R.layout.fragment_car_specific_details, container, false)
 
-        view.findViewById<TextView>(R.id.car_specific_nickname).text = car.nickname
-        view.findViewById<TextView>(R.id.car_specific_ymm_txt).text = "${car.year} ${car.make} ${car.model}"
-        view.findViewById<TextView>(R.id.vin_txt).text = car.VIN
-        view.findViewById<TextView>(R.id.license_plate_txt).text = car.licensePlate
-        view.findViewById<TextView>(R.id.license_state_txt).text = car.licenseState
+//        view.findViewById<TextView>(R.id.car_specific_nickname).text = car.nickname
+//        view.findViewById<TextView>(R.id.car_specific_ymm_txt).text = "${car.year} ${car.make} ${car.model}"
+//        view.findViewById<TextView>(R.id.vin_txt).text = car.VIN
+//        view.findViewById<TextView>(R.id.license_plate_txt).text = car.licensePlate
+//        view.findViewById<TextView>(R.id.license_state_txt).text = car.licenseState
 
-        var diagnosisView = view.findViewById<RecyclerView>(R.id.diagnosis_recycler) as RecyclerView
-        val adapter = DiagnosisAdapter()
-        diagnosisView.adapter = adapter
-        diagnosisView.layoutManager = LinearLayoutManager(context)
-        diagnosisView.setHasFixedSize(true)
+        //var diagnosisView = view.findViewById<RecyclerView>(R.id.diagnosis_recycler) as RecyclerView
 
-        return view
+
+//        val adapter = DiagnosisAdapter()
+//        diagnosisView.adapter = adapter
+//        diagnosisView.layoutManager = LinearLayoutManager(context)
+//        diagnosisView.setHasFixedSize(true)
+
+        return binding.root
     }
 
 }
