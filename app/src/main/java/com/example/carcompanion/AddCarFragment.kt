5b46862379563abb01103af4eda31a877468c5e8
nvonbulow <nick@nvonbulow.com>
2023-03-30 13:22:51 -0400

    Extract methods in add_car to dedicated functions for enhanced readability

--- app/src/main/java/com/example/carcompanion/AddCarFragment.kt
+++ app/src/main/java/com/example/carcompanion/AddCarFragment.kt
@@ -8,12 +8,9 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
-import com.google.android.gms.tasks.Task
-import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
 import com.example.carcompanion.ui.car_info.CarDetails
-//import kotlinx.android.synthetic.main.fragment_add_car.*
 
 class AddCarFragment(val user: String) : Fragment() {
 
@@ -22,43 +19,31 @@ class AddCarFragment(val user: String) : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_add_car, container, false)
-        view.findViewById<Button>(R.id.btn_add_car).setOnClickListener {
-            val car = CarDetails(
-                view.findViewById<EditText>(R.id.car_nickname_et).text.toString(),
-                    view.findViewById<EditText>(R.id.car_year_et).text.toString(),
-                    view.findViewById<EditText>(R.id.car_make_et).text.toString(),
-                    view.findViewById<EditText>(R.id.car_model_et).text.toString(),
-                    view.findViewById<EditText>(R.id.vin_txt).text.toString(),
-                    view.findViewById<EditText>(R.id.license_state_et).text.toString(),
-                    view.findViewById<EditText>(R.id.license_plate_et).text.toString()
-            )
-
-
-            val usersRef = FirebaseFirestore
-                    .getInstance()
-                    .collection("users")
-                    .document(user)
-            usersRef.get().addOnSuccessListener {
-                val userDoc = hashMapOf(
-                        "name" to "Mia"
-                )
-                usersRef.set(userDoc, SetOptions.merge())
-                        .addOnSuccessListener {
-                            Log.d(Constants.TAG, "added successfully!")
-                        }
-                        .addOnFailureListener {
-                            Log.d(Constants.TAG, "something went wrong with adding user :(")
-                        }
-            }
-
-            val carDocRef = FirebaseFirestore
-                    .getInstance()
-                    .collection("users")
-                    .document(user)
-                    .collection("cars")
-
-            carDocRef.add(car)
-        }
+        view.findViewById<Button>(R.id.btn_add_car).setOnClickListener { onAddCar(it) }
         return view
+    }
+
+    private fun onAddCar(view: View) {
+        val carDocRef = FirebaseFirestore
+            .getInstance()
+            .collection("users")
+            .document(user)
+            .collection("cars")
+
+        val car = getCarDetails(view)
+        carDocRef.add(car)
+    }
+
+    private fun getCarDetails(view: View): CarDetails {
+        val car = CarDetails(
+            view.findViewById<EditText>(R.id.car_nickname_et).text.toString(),
+            view.findViewById<EditText>(R.id.car_year_et).text.toString(),
+            view.findViewById<EditText>(R.id.car_make_et).text.toString(),
+            view.findViewById<EditText>(R.id.car_model_et).text.toString(),
+            view.findViewById<EditText>(R.id.vin_txt).text.toString(),
+            view.findViewById<EditText>(R.id.license_state_et).text.toString(),
+            view.findViewById<EditText>(R.id.license_plate_et).text.toString()
+        )
+        return car
     }
 }
