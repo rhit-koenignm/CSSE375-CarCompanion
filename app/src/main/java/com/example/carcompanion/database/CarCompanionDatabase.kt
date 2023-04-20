f94084f16ae4c0a6c37ad0d3380fd098ebeeb403
rhit-huy8 <78054590+rhit-huy8@users.noreply.github.com>
2023-04-19 20:13:00 -0400

    added test cases

--- app/src/main/java/com/example/carcompanion/database/CarCompanionDatabase.kt
+++ app/src/main/java/com/example/carcompanion/database/CarCompanionDatabase.kt
@@ -3,7 +3,7 @@ package com.example.carcompanion.database
 import com.example.carcompanion.ui.troubleshooting.TroubleData
 
 interface CarCompanionDatabase {
-    fun loadTroubleDouble(): ArrayList<TroubleData>
+    fun loadTroubleData(): ArrayList<TroubleData>
     fun loadIndicatorTroubleData(): ArrayList<TroubleData>
 
     fun loadDiagnosesTroubleData(): ArrayList<TroubleData>
